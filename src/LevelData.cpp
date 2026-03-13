09d3b10c25bd9662fd14dce595aaa4d207c9dd46
gab61201 <gab61201@gmail.com>
2026-03-13 16:59:43 +0800

    fix(level): remove unnecessary break statements in GetLevelData function

--- src/LevelData.cpp
+++ src/LevelData.cpp
@@ -2,6 +2,7 @@
 #include "Util/Text.hpp"
 
 static LevelData LevelData_1() {
+    
 }
 
 static LevelData LevelData_2() {
@@ -20,19 +21,13 @@ LevelData GetLevelData(LevelId levelId) {
     switch (levelId) {
         case LevelId::LEVEL_1:
             return LevelData_1();
-            break;
         case LevelId::LEVEL_2:
             return LevelData_2();
-            break;
         case LevelId::LEVEL_3:
             return LevelData_3();
-            break;
         case LevelId::LEVEL_4:
             return LevelData_4();
-            break;
         case LevelId::LEVEL_5:
             return LevelData_5();
-            break;
-        
     }
 }
