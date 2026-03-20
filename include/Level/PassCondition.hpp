21957a7600910bf783d3a263b803ef21d5c31d66
gab61201 <gab61201@gmail.com>
2026-03-20 15:00:24 +0800

    feat(level): Implement PassCondition class with contact handling for game physics

--- include/Level/PassCondition.hpp
+++ include/Level/PassCondition.hpp
@@ -1,19 +1,32 @@
 #ifndef PASS_CONDITION_HPP
 #define PASS_CONDITION_HPP
 
-#include "GameWorld/PhysicalWorld.hpp"
-#include "Util/GameObject.hpp"
+#include <box2d/b2_fixture.h>
+#include <box2d/b2_world_callbacks.h>
 
 enum class Condition {
     TOUCHING,
-    SEPERATED,
-    OVERLAP
+    SEPERATED
 };
 
-class PassCondition {
+class PassCondition : public b2ContactListener {
    public:
-    PassCondition();
-    bool Check();
+    PassCondition(b2Fixture* fixtureA, b2Fixture* fixtureB, int time, Condition condition);
+    ~PassCondition() = default;
+
+    void BeginContact(b2Contact* contact) override;
+    void EndContact(b2Contact* contact) override;
+
+    bool Check() const;
+
+   private:
+    b2Fixture* m_Fixture_A;
+    b2Fixture* m_Fixture_B;
+    int m_Time;
+    Condition m_Condition;
+
+    bool m_IsPassed = false;
+    int m_Counter = 0;
 };
 
 #endif
--- src/Level/PassCondition.cpp
+++ src/Level/PassCondition.cpp
@@ -1,2 +1,50 @@
 #include "Level/PassCondition.hpp"
 
+#include <box2d/b2_contact.h>
+
+#define FPS 60
+
+PassCondition::PassCondition(b2Fixture* fixtureA, b2Fixture* fixtureB, int time, Condition condition)
+    : m_Fixture_A(fixtureA), m_Fixture_B(fixtureB), m_Time(time * FPS), m_Condition(condition) {}
+
+bool PassCondition::Check() const {
+    return m_IsPassed;
+}
+
+void PassCondition::BeginContact(b2Contact* contact) {
+    if (m_IsPassed) {
+        return;
+    }
+
+    if ((m_Fixture_A == contact->GetFixtureA() && m_Fixture_B == contact->GetFixtureB()) ||
+        (m_Fixture_A == contact->GetFixtureB() && m_Fixture_B == contact->GetFixtureA())) {
+        if (m_Condition == Condition::TOUCHING) {
+            m_Counter++;
+        } else if (m_Condition == Condition::SEPERATED) {
+            m_Counter = 0;
+        }
+        
+        if (m_Counter >= m_Time) {
+            m_IsPassed = true;
+        }
+    }
+}
+
+void PassCondition::EndContact(b2Contact * contact) {
+    if (m_IsPassed) {
+        return;
+    }
+
+    if ((m_Fixture_A == contact->GetFixtureA() && m_Fixture_B == contact->GetFixtureB()) ||
+        (m_Fixture_A == contact->GetFixtureB() && m_Fixture_B == contact->GetFixtureA())) {
+        if (m_Condition == Condition::TOUCHING) {
+            m_Counter = 0;
+        } else if (m_Condition == Condition::SEPERATED) {
+            m_Counter++;
+        }
+
+        if (m_Counter >= m_Time) {
+            m_IsPassed = true;
+        }
+    }
+}
