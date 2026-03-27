1ab91b1b7dd2286f66b3ab38bea5c5ca0c9eaa63
gab61201 <gab61201@gmail.com>
2026-03-27 03:58:59 +0000

    feat(UI): Add backButton and resetButton to GameScreen for navigation

--- src/Screen/GameScreen.cpp
+++ src/Screen/GameScreen.cpp
@@ -6,6 +6,14 @@ GameScreen::GameScreen(LevelId* levelId) : m_Level(*levelId) {
     auto background = UI::Element::Background("Resources/Images/background.png");
     m_Renderer.AddChild(background);
 
+    auto backButton = UI::Element::CircleButton([]{});
+    backButton->m_Transform.translation ={-550.0f, -300.0f};
+    m_Renderer.AddChild(backButton);
+
+    auto resetButton = UI::Element::CircleButton([]{});
+    resetButton->m_Transform.translation ={550.0f, -300.0f};
+    m_Renderer.AddChild(resetButton);
+
     auto title = std::make_shared<Util::GameObject>();
     title->SetDrawable(
         std::make_shared<Util::Text>("PTSD/assets/fonts/Inter.ttf", 48, "GameScreen"));
