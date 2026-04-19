d66f3c1031345327dc538f19e5a47fc1b0b2a106
gab61201 <gab61201@gmail.com>
2026-04-20 01:39:54 +0800

    fix(Boundary): streamline Boundary constructor by removing unnecessary shape vector and initializing directly

--- src/GameWorld/CompositeObject/Boundary.cpp
+++ src/GameWorld/CompositeObject/Boundary.cpp
@@ -11,7 +11,8 @@ std::string LEVEL_IMAGE_PATH = "Resources/Images/level_background.png";
 
 namespace GameWorld {
 
-Boundary::Boundary(float x1, float x2, float y1, float y2) {
+Boundary::Boundary(float x1, float x2, float y1, float y2)
+    : CompositeObject(std::vector<std::shared_ptr<GameWorld::Shape>>()), m_X1(x1), m_X2(x2), m_Y1(y1), m_Y2(y2) {
     constexpr float THICKNESS = 10.0F;
 
     float centerX = (x1 + x2) * 0.5F;
@@ -19,27 +20,23 @@ Boundary::Boundary(float x1, float x2, float y1, float y2) {
     float width = x2 - x1;
     float height = y2 - y1;
 
-    std::vector<std::shared_ptr<Shape>> shapes;
-
     // Left wall
-    shapes.push_back(std::make_shared<Rectangle>(
+    m_Shapes.push_back(std::make_shared<Rectangle>(
         glm::vec2(THICKNESS, height), glm::vec2(x1 + THICKNESS * 0.5F, centerY)));
 
     // Right wall
-    shapes.push_back(std::make_shared<Rectangle>(
+    m_Shapes.push_back(std::make_shared<Rectangle>(
         glm::vec2(THICKNESS, height), glm::vec2(x2 - THICKNESS * 0.5F, centerY)));
 
     // Top wall
-    shapes.push_back(std::make_shared<Rectangle>(
+    m_Shapes.push_back(std::make_shared<Rectangle>(
         glm::vec2(width - THICKNESS * 2.0F, THICKNESS), glm::vec2(centerX, y2 - THICKNESS * 0.5F)));
 
     // Bottom wall
-    shapes.push_back(std::make_shared<Rectangle>(
+    m_Shapes.push_back(std::make_shared<Rectangle>(
         glm::vec2(width - THICKNESS * 2.0F, THICKNESS), glm::vec2(centerX, y1 + THICKNESS * 0.5F)));
 
-    m_Shapes = std::move(shapes);
     m_BodyType = BodyType::STATIC;
-    m_X1 = x1; m_X2 = x2; m_Y1 = y1; m_Y2 = y2;
 
     auto levelImage = std::make_shared<Util::Image>(LEVEL_IMAGE_PATH);
     auto boundaryVisual = std::make_shared<Util::GameObject>(levelImage, -0.1F);
