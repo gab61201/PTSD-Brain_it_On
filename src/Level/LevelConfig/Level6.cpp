c011534a22b6879934e12b2a766c161d563ab43c
Brady <t113820026@ntut.org.tw>
2026-06-16 18:24:57 +0800

    feat(level): add Level6 config

---
+++ src/Level/LevelConfig/Level6.cpp
@@ -0,0 +1,32 @@
+#include "GameWorld/CompositeObject/Boundary.hpp"
+#include "Level/LevelData.hpp"
+#include "Level/PassCondition/OneToOneContactPass.hpp"
+
+LevelConfig LevelConfig_6() {
+    LevelConfig data;
+    data.timeout = 10.0F;
+    data.strokeLimit = 1;
+    data.targetText = "Draw a shape";
+
+    auto boundary = std::make_shared<GameWorld::Boundary>();
+
+    std::vector<std::shared_ptr<GameWorld::CompositeObject>> objects = {boundary};
+
+    data.world = std::make_shared<GameWorld::PhysicalWorld>(objects, boundary);
+    data.passCondition = std::make_shared<OneToOneContactPass>(
+        boundary->GetBottomWall().Getb2ShapeId(),
+        TriggerType::TOUCHING, 0);
+
+    b2Shape_EnableContactEvents(boundary->GetBottomWall().Getb2ShapeId(), true);
+
+    return data;
+}
+
+namespace {
+struct Register {
+    Register() {
+        RegisterLevel(LevelId::LEVEL_6, LevelConfig_6);
+    }
+};
+static Register reg;
+}  // namespace
